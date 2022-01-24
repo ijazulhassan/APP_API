44de8c3afc485bfa333cf2c241a56b0e76fe01ab
Ijaz Ul Hassan <ijaz.h@pacsquare.com>
2022-01-24 08:18:45 +0500

    2nd commit

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,55 @@
+﻿using API.Data;
+using API.Entitites;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext Context)
+        {
+            _context = Context;
+        }
+        // GET: api/GellAll Users
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+          
+        // GET api/user/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUser(int id)
+        {
+            return await _context.Users.FindAsync(id);
+            
+        }
+
+        // POST api/<UsersController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<UsersController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<UsersController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ API/Entitites/User.cs
@@ -0,0 +1,10 @@
+﻿namespace API.Entitites
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+
+
+    }
+}
